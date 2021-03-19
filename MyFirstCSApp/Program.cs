e5007b5f0fdf6124ffe13b9691f6aeed0107557a
Raluca Rosu <raluca.rosu@amdaris.com>
2021-03-19 13:51:10 +0200

    Add VS project

---
+++ MyFirstCSApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstCSApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
