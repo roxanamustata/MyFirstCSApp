b9d7dac23684f75cb9afbf00d7f0ca7190dc4e5e
Raluca Rosu <raluca.rosu@amdaris.com>
2021-03-25 17:35:38 +0200

    Add comments to the project ClassesInCS

--- ClassesInCS/Program.cs
+++ ClassesInCS/Program.cs
@@ -11,20 +11,24 @@ namespace ClassesInCS
 
         static void Main(string[] args)
         {
+                        
+            //Circle and Rectangle are inheriting Unit, which is an abstract class that implements IArea interface
             Unit CircleUnit = new Circle() { Radius = 10 };
             Unit RectangleUnit = new Rectangle() { Length = 11, Width = 10 };
             Unit AnotherCircle = new Circle() { Radius = 20 };
 
+            //Each floor of a building is comprised of several units, each unit having a different shape
             Floor GroundFloor = new Floor();
 
             GroundFloor.Units.Add(CircleUnit);
             GroundFloor.Units.Add(RectangleUnit);
 
+            //Below I show method overriding as for each unit, the area is calculated depending on units shape 
             GroundFloor.CalculateArea();
             Console.ReadLine();
 
 
-
+            // Below I show methods overloading
             Floor.SimulateFloor(CircleUnit);
             Console.ReadLine();
             Floor.SimulateFloor(CircleUnit, RectangleUnit);
