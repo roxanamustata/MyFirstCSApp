b14035322c63dcb5922af453ddbe6aa145427343
Raluca Rosu <raluca.rosu@amdaris.com>
2021-03-25 16:41:38 +0200

    Add comments to Basics project

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -9,46 +9,58 @@ namespace Basics
             //Console.WriteLine("Hello World!");
 
 
-            // value types, reference types, ref, out
-            //Point p = new Point();
-            //p.x = 3;
-            //p.y = 3;
-            //Console.WriteLine(p.x + ", " + p.y);
+            // value types, reference types with class object
+            Point p = new Point();
+            p.x = 3;
+            p.y = 3;
 
-            //Point anotherP = new Point();
-            //Origin anotherS = new Origin();
+            //prints: 3, 3
+            Console.WriteLine(p.x + ", " + p.y);       
 
-            //modify(p);
-            //Console.WriteLine("Modified p without ref: "+p.x + ", " + p.y);
+            //prints 3, 3 because the reference of p has not changed
+            modify(p);
+            Console.WriteLine("Modified p without ref: " + p.x + ", " + p.y);
 
-            //modifyRef(ref p);
-            //Console.WriteLine("Modified p with ref: " + p.x + ", " + p.y);
+            //prints 1, 1 because now p is pointing to the object created in the method
+            modifyRef(ref p);
+            Console.WriteLine("Modified p with ref: " + p.x + ", " + p.y);
 
-            //Origin s=new Origin{ x=10, y=10};
-            //Console.WriteLine(s.x + ", " + s.y);
+            //value types, reference types with struct object
+            Origin s = new Origin { x = 10, y = 10 };
+            Console.WriteLine(s.x + ", " + s.y);
 
-            //modifyStruct(s);
-            //Console.WriteLine("Modified s without ref: " + s.x + ", " + s.y);
+            //prints 10, 10
+            modifyStruct(s);
+            Console.WriteLine("Modified s without ref: " + s.x + ", " + s.y);
 
-            //modifyStructRef(ref s);
-            //Console.WriteLine("Modified s with ref: " + s.x + ", " + s.y);
+            //prints 0, 0 as s will reference the new object created within the method
+            modifyStructRef(ref s);
+            Console.WriteLine("Modified s with ref: " + s.x + ", " + s.y);
 
-            //modifyOut(out anotherP);
-            //Console.WriteLine("Modified anotherP with out: " + anotherP.x + ", " + anotherP.y);
+            //out 
+            Point AnotherP = new Point();
+            Origin AnotherS = new Origin();
 
-            //modifyStructOut(out anotherS);
-            //Console.WriteLine("Modified anotherS with out: " + anotherS.x + ", " + anotherS.y);
+            //prints 1, 1 both for class and struct object as the reference will be to the new object created within the method
+            modifyOut(out AnotherP);
+            Console.WriteLine("Modified anotherP with out: " + AnotherP.x + ", " + AnotherP.y);
+
+            modifyStructOut(out AnotherS);
+            Console.WriteLine("Modified anotherS with out: " + AnotherS.x + ", " + AnotherS.y);
 
 
-            //boxing 
-            //int a = 100;
-            //object obj = a;
-            //Console.WriteLine("value of obj is: " + obj);
+
+            //boxing
+            //value of obj is 100
+            int a = 100;
+            object obj = a;
+            Console.WriteLine("value of obj is: " + obj);
 
             //unboxing
-            //int b =(int) obj;
-            //Console.WriteLine("value of b is: " + b);
-
+            //if I don't cast obj y int, I get an error
+            //value of b is 100
+            int b = (int)obj;
+            Console.WriteLine("value of b is: " + b);
 
 
             Console.ReadLine();
