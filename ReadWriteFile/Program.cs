c1596f438537e25c8466da2ec2126077e9d0ab7c
Raluca Rosu <raluca.rosu@amdaris.com>
2021-04-04 08:05:12 +0300

    Add ReadWriteFile project

---
+++ ReadWriteFile/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace ReadWriteFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter file path:");
+            string path = Console.ReadLine();
+
+
+            //Write in file the lines read from the console
+            try
+            {
+                //add parameter true if for appending to file, without overwriting
+                var writer = new StreamWriter(path, true);
+                string line;
+
+                while ((line = Console.ReadLine()) != "exit")
+                {
+                    writer.WriteLine(line);
+                }
+
+                writer.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+
+            // Read lines from file and show them on console
+            try
+            {
+                var reader = new StreamReader(path);
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+
+                reader.Close();
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+        }
+
+    }
+}
+
