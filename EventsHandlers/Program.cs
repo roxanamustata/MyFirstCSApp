c0c526770cb6dcbe35c5cb491d8ee7df83002bad
Ralu <roxanamustata@yahoo.com>
2021-04-07 15:02:28 +0300

    Change on EventsHandlers project

--- EventsHandlers/Program.cs
+++ EventsHandlers/Program.cs
@@ -8,7 +8,8 @@ namespace EventsHandlers
         {
             IEventTrigger trigger = new Course(new DateTime(2021, 5, 1, 0, 0, 0));
 
-            trigger.OnStartDateChanged += (o, e) => Console.WriteLine($"Announce all users about new start date: {e.Value}");
+            trigger.OnStartDateChanged += (o, e) => Console.WriteLine($"Send email to all users about new start date: {e.Value}");
+            trigger.OnStartDateChanged += (o, e) => Console.WriteLine($"Publish on social media the new start date: {e.Value}");
             trigger.StartDate = new DateTime(2021, 5, 2, 0, 0, 0);
         }
     }
